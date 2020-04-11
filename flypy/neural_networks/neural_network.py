033d01057c2a48cec3c79fa6debfac3f17b48585
Bobby Scharmann <Bobby.Scharmann@gmail.com>
2020-04-10 20:34:07 -0500

    two layer neural network is broken - going to do some code refactoring

--- flypy/neural_networks/neural_network.py
+++ flypy/neural_networks/neural_network.py
@@ -19,13 +19,19 @@ class NeuralNetworkTwoLayers(object):
 
         # activation function
         self.activation = activation
+
+        # The gradient vector has a direction and a magnitude. Gradient descent algorithms 
+        # multiply the magnitude of the gradient by a scalar known as learning rate (also 
+        # sometimes called step size) to determine the next point.
         self.learning_rate = learning_rate
         
     @classmethod
-    def cost(cls, ypred: np.float, y_actual: np.float):
+    def cost(cls, ypred: np.ndarray, y_actual: np.ndarray):
        """Cost function is the mean square error (MSE) """
-       
-       return np.sqrt(ypred**2 + y_actual**2)
+       m = ypred.shape[1]
+       J = -(1 / m) * np.sum([y_actual[i] * np.log10(ypred[i]) + (1-y_actual[i]) * np.log10(1-ypred[i]) for i in range(m)])
+       return J
+       #return np.sqrt(ypred**2 + y_actual**2)
    
     @property
     def inputs(self) -> None:
@@ -47,18 +53,35 @@ class NeuralNetworkTwoLayers(object):
         # initialize weights and biases
         # TODO: 3 here should be dynamically computed based on the number
         #       of neurons on the input layer
-        self.__biases = np.zeros((3,1))
+        
+        # Number of output neurons
+        n = 2
+        
+        # Number of input neurons
+        m = 4
+        self.__biases = np.zeros((n,1))
 
         # Initialize weights to a random number with a mean of 0.5
-        self.__weights = 2 * np.random.random((3,1)) - 1
+        self.__weights = 2 * np.random.random((n,m)) - 1
         
-        # Forward Propagation
-        z = np.dot(self.inputs, self.__weights) + self.__biases
-        a = self.activation.eval(z)
-        
-        # Calculate Loss
-        error = self.cost(a, self.outputs) 
-        
-        # Backward Propagation
-        
-        # Gradient Descent 
+        for i in range(epochs):
+            # Forward Propagation
+            # NOTE: Might need to transpose inputs here
+            z = np.dot(self.__inputs.T, self.__weights.T) + self.__biases
+            A = self.activation.eval(z)
+            
+            # Calculate Loss
+            error = self.cost(A, self.__outputs) 
+            error = np.squeeze(error) 
+            # Backward Propagation
+
+            # Gradient Descent 
+            # partial derivatives of weight and bias w.r.t cost
+            dw = (1 / m) * np.dot(self.__inputs.T, (A - self.__outputs).T)
+            db =  (1 / m) * A - self.__outputs
+            
+            # Update the weights
+            self.__weights -= self.learning_rate * dw
+            self.__biases -= self.learning_rate * db
+            #if i % 100 == 0:
+            print("Epoch: ", i, " \tcost: ", error) 
