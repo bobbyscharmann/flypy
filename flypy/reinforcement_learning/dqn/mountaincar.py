d567b0e4c1e338ed05bb249a886fd4a1bb24fcd9
Bobby Scharmann <Bobby.Scharmann@gmail.com>
2020-04-26 14:54:45 -0500

    imporovements to RL MountainCartv0

--- flypy/reinforcement_learning/dqn/mountaincar.py
+++ flypy/reinforcement_learning/dqn/mountaincar.py
@@ -1,14 +1,21 @@
 import gym
+import numpy as np
 
 env = gym.make("MountainCar-v0")
 env.reset()
+
+NUM_POSSIBLE_ACTIONS = env.action_space.n
+NUM_POSSIBLE_STATES = len(env.observation_space.high)
+q_space = np.zeros((NUM_POSSIBLE_STATES, NUM_POSSIBLE_ACTIONS))
+
+# Ratio between exploring and exploiting the environment
+EPSILON = 0.25
 
 done = False
 
 while not done:
     print(env.observation_space.high)
     print(env.observation_space.low)
-    print(env.action_space.n)
     action = 2
     new_state, reward, done, _ = env.step(action)
     env.render()
