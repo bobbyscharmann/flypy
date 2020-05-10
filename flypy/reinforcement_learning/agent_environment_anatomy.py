e48ce75783ecf81dc284b5d15f9a1319a3a97a1f
Bobby Scharmann <Bobby.Scharmann@gmail.com>
2020-05-10 16:19:19 -0500

    simple RL agent and environment - revision 1

---
+++ flypy/reinforcement_learning/agent_environment_anatomy.py
@@ -0,0 +1,41 @@
+import random
+
+
+class Environment(object):
+    steps_remaining: int
+
+    def __init__(self):
+        self.steps_remaining = 10
+        return
+
+    def action(self, action: int) -> float:
+        reward = random.random()
+        self.steps_remaining -= 1
+        return reward
+
+    def get_observations(self):
+        return [0.0, 0.0, 0.0]
+
+    @staticmethod
+    def get_actions(self):
+        return [0, 1]
+
+    def is_done(self) -> bool:
+        return self.steps_remaining == 0
+
+
+class Agent(object):
+    def __init__(self, env: Environment):
+        self.environment = env
+        return
+
+    def step(self):
+        return random.choice(self.environment.get_observations())
+
+
+if __name__ == "__main__":
+    env = Environment()
+    agent = Agent(env)
+
+    while not env.is_done():
+        print(agent.step())
