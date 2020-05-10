0c886a3c488a767d88fbb06474867adee2113802
Bobby Scharmann <Bobby.Scharmann@gmail.com>
2020-05-10 16:32:58 -0500

    Simple RL agent and environment - now at least poorly commented

--- flypy/reinforcement_learning/agent_environment_anatomy.py
+++ flypy/reinforcement_learning/agent_environment_anatomy.py
@@ -9,18 +9,24 @@ class Environment(object):
         return
 
     def action(self, action: int) -> float:
+        """Handles the Agent's action and returns the reward for said action"""
         reward = random.random()
+        if self.is_done():
+            raise Exception("Game over!")
         self.steps_remaining -= 1
         return reward
 
-    def get_observations(self):
+    def get_observation(self):
+        """Environments current observations for the Agent. This could be positions, velocities, etc."""
         return [0.0, 0.0, 0.0]
 
     @staticmethod
     def get_actions(self):
+        """List of actions available for execution"""
         return [0, 1]
 
     def is_done(self) -> bool:
+        """Done with this episode? Number of steps defined in steps_remaining class variable"""
         return self.steps_remaining == 0
 
 
@@ -30,7 +36,7 @@ class Agent(object):
         return
 
     def step(self):
-        return env.action(random.choice(self.environment.get_observations()))
+        return env.action(random.choice(self.environment.get_observation()))
 
 
 if __name__ == "__main__":
