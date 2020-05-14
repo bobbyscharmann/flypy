920eec6c4102d3e453d7f8fcd9c62efba281e1d5
Bobby Scharmann <Bobby.Scharmann@gmail.com>
2020-05-13 22:45:34 -0500

    Pytorch simple neural network

--- examples/tensor_play.py
+++ examples/tensor_play.py
@@ -3,8 +3,9 @@ import torch
 # Create a few tensors on GPU
 V1 = torch.tensor([1.0, 2.0], requires_grad=True, device='cuda:0')
 V2 = torch.tensor([1.0, 1.0], requires_grad=False, device='cuda:0')
+V3 = torch.tensor([1.0, 1.0], requires_grad=True, device='cuda:0')
 
-v_sum = 2 * (V1 + V2)
+v_sum = 2 * (V1 + V2 + V3)
 sum = (v_sum * 3).sum()
 print(f"Sum is: {sum}")
 
@@ -12,3 +13,27 @@ print(f"Sum is: {sum}")
 a = sum.backward()
 print(f"V1.grad Gradient: {V1.grad}")
 print(f"V2.grad Gradient: {V2.grad}")
+print(f"V3.grad Gradient: {V3.grad}")
+
+
+class BobsNN(torch.nn.Module):
+    def __init__(self, n_inputs, n_outputs):
+
+        super().__init__()
+        self.pipe = torch.nn.Sequential(torch.nn.Linear(n_inputs, 5),
+                                        torch.nn.ReLU(),
+                                        torch.nn.Linear(5, n_outputs),
+                                        torch.nn.ReLU(),
+                                        torch.nn.Dropout(p=0.2),
+                                        torch.nn.Softmax(dim=1))
+
+    def forward(self, X):
+        Y_hat = self.pipe(X)
+        return Y_hat
+
+
+
+X = torch.FloatTensor([[3, 3], [4, 4]])
+model = BobsNN(n_inputs=2, n_outputs=1)
+y_pred = model(X)
+print(f"Model predictions: {y_pred}")
