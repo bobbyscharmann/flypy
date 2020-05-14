29b41363f422eb2ecd94a1d54042a598ce511059
Bobby Scharmann <Bobby.Scharmann@gmail.com>
2020-05-13 22:29:40 -0500

    Moved tensors to GPU

--- examples/tensor_play.py
+++ examples/tensor_play.py
@@ -1,7 +1,8 @@
 import torch
 
-V1 = torch.tensor([1.0, 2.0], requires_grad=True)
-V2 = torch.tensor([1.0, 1.0], requires_grad=False)
+# Create a few tensors on GPU
+V1 = torch.tensor([1.0, 2.0], requires_grad=True, device='cuda:0')
+V2 = torch.tensor([1.0, 1.0], requires_grad=False, device='cuda:0')
 
 v_sum = 2 * (V1 + V2)
 sum = (v_sum * 3).sum()
