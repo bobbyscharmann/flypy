64a32230987a79c5857c166f071521cf90afe4ae
Bobby Scharmann <Bobby.Scharmann@gmail.com>
2020-05-14 22:44:54 -0500

    adding training step

--- examples/tensor_play.py
+++ examples/tensor_play.py
@@ -20,7 +20,7 @@ print(f"V3.grad Gradient: {V3.grad}")
 class BobsNN(torch.nn.Module):
     def __init__(self, n_inputs, n_outputs):
 
-        super().__init__()
+        super(BobsNN, self).__init__()
         self.pipe = torch.nn.Sequential(torch.nn.Linear(n_inputs, 5),
                                         torch.nn.ReLU(),
                                         torch.nn.Linear(5, n_outputs),
@@ -33,20 +33,27 @@ class BobsNN(torch.nn.Module):
         Y_hat = self.pipe(X)
         return Y_hat
 
-X = torch.rand(200, 2)
-model = BobsNN(n_inputs=2, n_outputs=1)
-y_pred = model(X)
-print(f"Model predictions: {y_pred}")
+x_vals = np.linspace(0,200,1)
+X = torch.tensor([[x_vals], [x_vals**2]])
+model = BobsNN(n_inputs=1, n_outputs=1)#.to(device='cuda:0')
+#print(f"Model predictions: {y_pred}")
 batch_size = 2
 num_epochs = 10
-X_train, Y_train, X_test, Y_test = train_test_split(X, test_size=0.2)
+X_train, X_test, Y_train, Y_test = train_test_split(X[:, 0], X[:, 1], test_size=0.2)
 
-for epoch in num_epochs:
+
+optimizer = torch.optim.Adam(params=model.parameters(), lr=0.01)
+criterion = torch.nn.MSELoss()
+for epoch in range(num_epochs):
     for batch in range(1, len(X_train), batch_size):
         X_batch = X_train[batch:batch+batch_size]
         Y_batch = Y_train[batch:batch+batch_size]
+        Y_hat = model(X_batch)
+        loss = criterion(Y_batch, Y_hat)
+        print(f"Loss: {loss}")
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
 
-        Y_hat = model(X_train)
-        loss = torch.nn.MSELoss(Y_hat - Y_batch)
-        loss
+
 
