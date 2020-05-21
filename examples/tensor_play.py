1f91b72bce5965f7267d77651edada380c596a95
Bobby Scharmann <Bobby.Scharmann@gmail.com>
2020-05-20 22:40:50 -0500

    Sine wave fully plots now

--- examples/tensor_play.py
+++ examples/tensor_play.py
@@ -1,10 +1,10 @@
+import imageio
+import matplotlib.pyplot as plt
 import numpy as np
+import os
 import torch
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-import matplotlib.pyplot as plt
-import imageio
-import os
 # Create a few tensors on GPU
 V1 = torch.tensor([1.0, 2.0], requires_grad=True, device='cuda:0')
 V2 = torch.tensor([1.0, 1.0], requires_grad=False, device='cuda:0')
@@ -26,12 +26,10 @@ class BobsNN(torch.nn.Module):
 
         super(BobsNN, self).__init__()
         self.pipe = torch.nn.Sequential(torch.nn.Linear(n_inputs, 100),
-                                        #torch.nn.clamp(min=0),
                                         torch.nn.ReLU(),
                                         torch.nn.Linear(100, 32),
                                         torch.nn.ReLU(),
                                         torch.nn.Linear(32, n_outputs),
-                                        #torch.nn.ReLU(),
                                         )
 
     def forward(self, x):
@@ -39,7 +37,16 @@ class BobsNN(torch.nn.Module):
         return Y_hat
 
 x_vals = np.linspace(0, 20, 2000)
-y_vals = np.sin(x_vals)#x_vals**3 + 2*x_vals**2 + 3
+y_vals = np.zeros(x_vals.shape)
+#for idx, x in enumerate(x_vals):
+#    if idx < len(x_vals) / 3:
+#        y_vals[idx] = np.sin(x)
+#    elif idx > 2 * len(x_vals) / 3:
+#        y_vals[idx] = 1
+#    else:
+#        y_vals[idx] = np.cos(x)
+
+y_vals = np.sin(x_vals)
 xscaler = MinMaxScaler()
 yscaler = MinMaxScaler()
 xscaler.fit(x_vals.reshape(-1, 1))
@@ -59,7 +66,7 @@ model = BobsNN(n_inputs=1, n_outputs=1)#.to(device='cuda:0')
 #model = TwoLayerNet(1, 100, 1)
 #print(f"Model predictions: {y_pred}")
 batch_size = 2000
-num_epochs = 4000
+num_epochs = 1000
 X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2, shuffle=True, random_state=42)
 print(f"X.train{X_train.shape}")
 print(f"Y.train{Y_train.shape}")
@@ -93,12 +100,12 @@ for epoch in range(num_epochs):
             plt.scatter(xscaler.inverse_transform(x_vals.detach().numpy().reshape(-1, 1)),
                         yscaler.inverse_transform(y_vals.detach().numpy().reshape(-1, 1)),
                         label="f_hat(X) (model estimate)")
-            x = X_orig[0, :].T  # torch.randn(2000, 1)
-            y = X_orig[1, :].T  # torch.randn(2000, 1)
+            x = X_orig[0, :].T
+            y = X_orig[1, :].T
             plt.scatter(xscaler.inverse_transform(x), yscaler.inverse_transform(y), label="f(X) (truth data)")
             plt.xlabel("X")
             plt.ylabel("Y")
-            plt.title(f"y=x^3 + 2x^2 + 3 @ epoch {epoch}")
+            plt.title(f"y=sin(x) @ epoch {epoch}")
             plt.legend()
             plt.savefig(os.path.join("results", f"epoch_{epoch}"))
             plt.close()
