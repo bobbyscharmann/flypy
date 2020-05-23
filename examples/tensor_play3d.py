b9392a4872b1cbd8e445f118bb4d484c6adc89fb
Bobby Scharmann <Bobby.Scharmann@gmail.com>
2020-05-23 10:36:40 -0500

    Getting really close...

--- examples/tensor_play3d.py
+++ examples/tensor_play3d.py
@@ -26,21 +26,19 @@ class BobsNN(torch.nn.Module):
         return Y_hat
 
 NUM_INPUT_FEATURES = 2
-NUM_SAMPLES = 2000         
-col1 = np.linspac e (0, 20, NUM_SAMPLES)
+NUM_SAMPLES = 200
+col1 = np.linspace (0, 20, NUM_SAMPLES)
 col2 = np.linspace (0, 20, NUM_SAMPLES)
-x_vals = np.zeros(NUM_SAMPLES ** 2, NUM_INPUT_FEATURES)
+x_vals = np.zeros((NUM_SAMPLES ** 2, NUM_INPUT_FEATURES))
 for idx1, x1 in enumerate(col1):
     for idx2, x2 in enumerate(col2):
-       x_vals[idx1 * NUM_SAMPLES, ]
+        x_vals[idx1 * NUM_SAMPLES + idx2, 0] = x1
+        x_vals[idx1 * NUM_SAMPLES + idx2, 1] = x2
 
-x_vals = np.vstack((np.linspace(0, 20, 2000), np.linspace(0, 20, 2000))).T
-y_vals = np.zeros((x_vals.shape[0], x_vals.shape[0]))
-count = 0
-for idx1, val in enumerate(x_vals[0, :]):
-    for idx2, item in enumerate(x_vals[1, :]):
-        y_vals[idx1, idx2] = np.sin(val) * 0.01 * item
-        count += 1
+#x_vals = np.vstack((np.linspace(0, 20, 2000), np.linspace(0, 20, 2000))).T
+y_vals = np.zeros((NUM_SAMPLES**2, 1))
+for idx, val in enumerate(x_vals):
+    y_vals[idx] = np.sin(val[0]) * 0.01 * val[1]
 #    elif idx > 2 * len(x_vals) / 3:
 #        y_vals[idx] = 1
 #    else:
@@ -49,18 +47,18 @@ for idx1, val in enumerate(x_vals[0, :]):
 #y_vals = np.sin(x_vals)
 xscaler = MinMaxScaler()
 yscaler = MinMaxScaler()
-xscaler.fit(x_vals.reshape(-1, 2))
-x_vals_scaled = xscaler.transform(x_vals.reshape(-1, 2))
+xscaler.fit(x_vals.reshape(-1, NUM_INPUT_FEATURES))
+x_vals_scaled = xscaler.transform(x_vals.reshape(-1, NUM_INPUT_FEATURES))
 yscaler.fit(y_vals.reshape(-1, 1))
 y_vals_scaled = yscaler.transform(y_vals.reshape(-1, 1))
-X_orig = torch.FloatTensor((x_vals_scaled.reshape(2, -1)))
+X_orig = torch.FloatTensor((x_vals_scaled.reshape(NUM_INPUT_FEATURES, -1)))
 #X_orig = torch.FloatTensor(np.vstack((x_vals_scaled.reshape(2, -1), y_vals_scaled.reshape(2, 1))))
 x = X_orig[0:2, :].T
 y = torch.FloatTensor(y_vals_scaled)#X_orig[2, :].T
 
-model = BobsNN(n_inputs=2, n_outputs=1)#.to(device='cuda:0')
-batch_size = 2000
-num_epochs = 100
+model = BobsNN(n_inputs=NUM_INPUT_FEATURES, n_outputs=1)#.to(device='cuda:0')
+batch_size = NUM_SAMPLES**2
+num_epochs = 500
 #X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2, shuffle=True, random_state=42)
 X_train = x
 Y_train = y
@@ -86,35 +84,39 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
         X_batch = X_train[batch:batch+batch_size]
         Y_batch = Y_train[batch:batch+batch_size]
-        Y_train = Y_train.view(Y_train.shape[0], -1)
-        Y_hat = model(X_batch.view(X_batch.shape[0], -1))
+        #Y_train = Y_train.view(batch_size, 1)
+        #Y_train = Y_train.view(Y_train.shape[0], -1)
+        #Y_hat = model(X_batch.view(X_batch.shape[0], -1))
+        Y_hat = model(X_batch)
         loss = criterion(Y_hat, Y_batch)
-        if epoch % 100 == 0:
+        if epoch % 10 == 0:
             print(f"Epoch: {epoch}, Batch: {batch_num}, Loss: {loss.item()}")
         loss.backward()
         optimizer.step()
 
         # Every ten epochs, test out the model
         if epoch % 10 == 0:
-            x_vals = torch.FloatTensor(np.linspace(0, 1, 2000))
-            x_vals = np.vstack((np.linspace(0, 20, 2000), np.linspace(0, 20, 2000))).T
-            x_vals = x_vals.view(dtype=float)
-            y_vals = model(torch.FloatTensor(x_vals))
+            #x_vals = torch.FloatTensor(np.linspace(0, 1, NUM_SAMPLES))
+            #x_vals = np.vstack((np.linspace(0, 20, NUM_SAMPLES), np.linspace(0, 20, NUM_SAMPLES))).T
+            #x_vals = x_vals.view(dtype=float)
+            y_vals = model(X_train)
             fig = plt.figure()
             ax = fig.add_subplot(111, projection='3d')
 
-            tmp_x1 = xscaler.inverse_transform(x_vals.reshape(-1, 2))[:, 0]
-            tmp_x2 = xscaler.inverse_transform(x_vals.reshape(-1, 2))[:, 1]
-            tmp_y = yscaler.inverse_transform(y_vals.detach().numpy().reshape(-1, 1))
-            ax.scatter(tmp_x1,
-                        tmp_x2,
+            tmp_x1 = xscaler.inverse_transform(x_vals.reshape(-1, NUM_INPUT_FEATURES))[:, 0]
+            tmp_x2 = xscaler.inverse_transform(x_vals.reshape(-1, NUM_INPUT_FEATURES))[:, 1]
+            tmp_y = yscaler.inverse_transform(y_vals.detach().numpy()).reshape(NUM_SAMPLES, NUM_SAMPLES)
+            #tmp_y = yscaler.inverse_transform(y_vals.detach().numpy().reshape(NUM_SAMPLES, NUM_SAMPLES))
+            ax.scatter(x_vals[:, 0],
+                        x_vals[:, 1],
                         tmp_y,
                         label="f_hat(X) (model estimate)")
             x1 = X_orig[0, :].reshape(-1, 1)
             x2 = X_orig[1, :].reshape(-1, 1)
             #y = X_orig[2, :].T
-            tmp = xscaler.inverse_transform(X_orig[0:2, :].reshape(-1, 2))
+            tmp = xscaler.inverse_transform(X_orig[0:NUM_INPUT_FEATURES, :].reshape(-1, NUM_INPUT_FEATURES))
             new_y = yscaler.inverse_transform(y)
+            new_y.reshape(NUM_SAMPLES, NUM_SAMPLES)
             ax.scatter(tmp[:, 0],
                        tmp[:, 1],
                        new_y, label="f(X) (truth data)")
