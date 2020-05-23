24f412f4668768954c4b12d3cb15dc66eb787f50
Bobby Scharmann <Bobby.Scharmann@gmail.com>
2020-05-23 11:39:42 -0500

    Getting really close...

--- examples/tensor_play3d.py
+++ examples/tensor_play3d.py
@@ -16,7 +16,9 @@ class BobsNN(torch.nn.Module):
         super(BobsNN, self).__init__()
         self.pipe = torch.nn.Sequential(torch.nn.Linear(n_inputs, 100),
                                         torch.nn.ReLU(),
-                                        torch.nn.Linear(100, 32),
+                                        torch.nn.Linear(100, 64),
+                                        torch.nn.ReLU(),
+                                        torch.nn.Linear(64, 32),
                                         torch.nn.ReLU(),
                                         torch.nn.Linear(32, n_outputs),
                                         )
@@ -26,7 +28,7 @@ class BobsNN(torch.nn.Module):
         return Y_hat
 
 NUM_INPUT_FEATURES = 2
-NUM_SAMPLES = 200
+NUM_SAMPLES = 100
 col1 = np.linspace (0, 20, NUM_SAMPLES)
 col2 = np.linspace (0, 20, NUM_SAMPLES)
 x_vals = np.zeros((NUM_SAMPLES ** 2, NUM_INPUT_FEATURES))
@@ -38,7 +40,7 @@ for idx1, x1 in enumerate(col1):
 #x_vals = np.vstack((np.linspace(0, 20, 2000), np.linspace(0, 20, 2000))).T
 y_vals = np.zeros((NUM_SAMPLES**2, 1))
 for idx, val in enumerate(x_vals):
-    y_vals[idx] = np.sin(val[0]) * 0.01 * val[1]
+    y_vals[idx] = np.sin(val[0]) * 0.01 #* val[1]
 #    elif idx > 2 * len(x_vals) / 3:
 #        y_vals[idx] = 1
 #    else:
@@ -99,31 +101,36 @@ for epoch in range(num_epochs):
             #x_vals = torch.FloatTensor(np.linspace(0, 1, NUM_SAMPLES))
             #x_vals = np.vstack((np.linspace(0, 20, NUM_SAMPLES), np.linspace(0, 20, NUM_SAMPLES))).T
             #x_vals = x_vals.view(dtype=float)
-            y_vals = model(X_train)
-            fig = plt.figure()
+            y_vals = model(X_train.view(-1, NUM_INPUT_FEATURES))
+            #y_vals = model(X_train)
+            fig = plt.figure(figsize=(13,11))
             ax = fig.add_subplot(111, projection='3d')
-
-            tmp_x1 = xscaler.inverse_transform(x_vals.reshape(-1, NUM_INPUT_FEATURES))[:, 0]
-            tmp_x2 = xscaler.inverse_transform(x_vals.reshape(-1, NUM_INPUT_FEATURES))[:, 1]
-            tmp_y = yscaler.inverse_transform(y_vals.detach().numpy()).reshape(NUM_SAMPLES, NUM_SAMPLES)
+            #print(f" Predicted: {y_vals[420]} and was: {Y_train[420]}")
+            #tmp_x1 = xscaler.inverse_transform(x_vals.reshape(-1, NUM_INPUT_FEATURES))[:, 0]
+            #tmp_x2 = xscaler.inverse_transform(x_vals.reshape(-1, NUM_INPUT_FEATURES))[:, 1]
+            tmp_x1 = x_vals[:, 0]
+            tmp_x2 = x_vals[:, 1]
+            #tmp_y = yscaler.inverse_transform(y_vals.detach().numpy())
+            tmp_y = np.zeros(y_vals.shape)#y_vals.detach().numpy()
+            #tmp_y = tmp_y.reshape(NUM_SAMPLES, NUM_SAMPLES)
+            #tmp_y = y_vals.detach().numpy().reshape(NUM_SAMPLES, NUM_SAMPLES)
             #tmp_y = yscaler.inverse_transform(y_vals.detach().numpy().reshape(NUM_SAMPLES, NUM_SAMPLES))
-            ax.scatter(x_vals[:, 0],
-                        x_vals[:, 1],
-                        tmp_y,
-                        label="f_hat(X) (model estimate)")
+            ax.scatter(tmp_x1,
+                       tmp_x2,
+                       tmp_y,
+                       label="f_hat(X) (model estimate)")
             x1 = X_orig[0, :].reshape(-1, 1)
             x2 = X_orig[1, :].reshape(-1, 1)
             #y = X_orig[2, :].T
             tmp = xscaler.inverse_transform(X_orig[0:NUM_INPUT_FEATURES, :].reshape(-1, NUM_INPUT_FEATURES))
-            new_y = yscaler.inverse_transform(y)
+            new_y = y#yscaler.inverse_transform(y)
             new_y.reshape(NUM_SAMPLES, NUM_SAMPLES)
-            ax.scatter(tmp[:, 0],
-                       tmp[:, 1],
-                       new_y, label="f(X) (truth data)")
+            #ax.scatter(tmp[:, 0], tmp[:, 1], new_y, label="f(X) (truth data)")
             plt.xlabel("X")
             plt.ylabel("Y")
             plt.title(f"y=sin(x) @ epoch {epoch}")
             plt.legend()
+            #plt.show()
             plt.savefig(os.path.join("results", f"epoch_{epoch}"))
             plt.close()
 
