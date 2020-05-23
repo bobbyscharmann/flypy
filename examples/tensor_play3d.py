e5331c950ee1534c0c73a529315d337063067647
Bobby Scharmann <Bobby.Scharmann@gmail.com>
2020-05-23 12:41:39 -0500

    CLoser, maybe?

--- examples/tensor_play3d.py
+++ examples/tensor_play3d.py
@@ -16,9 +16,7 @@ class BobsNN(torch.nn.Module):
         super(BobsNN, self).__init__()
         self.pipe = torch.nn.Sequential(torch.nn.Linear(n_inputs, 100),
                                         torch.nn.ReLU(),
-                                        torch.nn.Linear(100, 64),
-                                        torch.nn.ReLU(),
-                                        torch.nn.Linear(64, 32),
+                                        torch.nn.Linear(100, 32),
                                         torch.nn.ReLU(),
                                         torch.nn.Linear(32, n_outputs),
                                         )
@@ -32,15 +30,17 @@ NUM_SAMPLES = 100
 col1 = np.linspace (0, 20, NUM_SAMPLES)
 col2 = np.linspace (0, 20, NUM_SAMPLES)
 x_vals = np.zeros((NUM_SAMPLES ** 2, NUM_INPUT_FEATURES))
+count = 0
 for idx1, x1 in enumerate(col1):
     for idx2, x2 in enumerate(col2):
-        x_vals[idx1 * NUM_SAMPLES + idx2, 0] = x1
-        x_vals[idx1 * NUM_SAMPLES + idx2, 1] = x2
+        x_vals[count, 0] = x1
+        x_vals[count, 1] = x2
+        count += 1
 
 #x_vals = np.vstack((np.linspace(0, 20, 2000), np.linspace(0, 20, 2000))).T
 y_vals = np.zeros((NUM_SAMPLES**2, 1))
 for idx, val in enumerate(x_vals):
-    y_vals[idx] = np.sin(val[0]) * 0.01 #* val[1]
+    y_vals[idx] = 2 * (np.cos(val[0])) + 0.1 * val[1]
 #    elif idx > 2 * len(x_vals) / 3:
 #        y_vals[idx] = 1
 #    else:
@@ -60,7 +60,7 @@ y = torch.FloatTensor(y_vals_scaled)#X_orig[2, :].T
 
 model = BobsNN(n_inputs=NUM_INPUT_FEATURES, n_outputs=1)#.to(device='cuda:0')
 batch_size = NUM_SAMPLES**2
-num_epochs = 500
+num_epochs = 20000
 #X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2, shuffle=True, random_state=42)
 X_train = x
 Y_train = y
@@ -86,9 +86,6 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
         X_batch = X_train[batch:batch+batch_size]
         Y_batch = Y_train[batch:batch+batch_size]
-        #Y_train = Y_train.view(batch_size, 1)
-        #Y_train = Y_train.view(Y_train.shape[0], -1)
-        #Y_hat = model(X_batch.view(X_batch.shape[0], -1))
         Y_hat = model(X_batch)
         loss = criterion(Y_hat, Y_batch)
         if epoch % 10 == 0:
@@ -97,12 +94,8 @@ for epoch in range(num_epochs):
         optimizer.step()
 
         # Every ten epochs, test out the model
-        if epoch % 10 == 0:
-            #x_vals = torch.FloatTensor(np.linspace(0, 1, NUM_SAMPLES))
-            #x_vals = np.vstack((np.linspace(0, 20, NUM_SAMPLES), np.linspace(0, 20, NUM_SAMPLES))).T
-            #x_vals = x_vals.view(dtype=float)
-            y_vals = model(X_train.view(-1, NUM_INPUT_FEATURES))
-            #y_vals = model(X_train)
+        if epoch % (num_epochs / 100) == 0:
+            y_vals = model(X_train)
             fig = plt.figure(figsize=(13,11))
             ax = fig.add_subplot(111, projection='3d')
             #print(f" Predicted: {y_vals[420]} and was: {Y_train[420]}")
@@ -111,13 +104,18 @@ for epoch in range(num_epochs):
             tmp_x1 = x_vals[:, 0]
             tmp_x2 = x_vals[:, 1]
             #tmp_y = yscaler.inverse_transform(y_vals.detach().numpy())
-            tmp_y = np.zeros(y_vals.shape)#y_vals.detach().numpy()
+            tmp_y = y_vals.detach().numpy()
             #tmp_y = tmp_y.reshape(NUM_SAMPLES, NUM_SAMPLES)
             #tmp_y = y_vals.detach().numpy().reshape(NUM_SAMPLES, NUM_SAMPLES)
             #tmp_y = yscaler.inverse_transform(y_vals.detach().numpy().reshape(NUM_SAMPLES, NUM_SAMPLES))
-            ax.scatter(tmp_x1,
-                       tmp_x2,
+            #ax.scatter(tmp_x1,
+            #           tmp_x2,
+            #           tmp_y,
+            #           label="f_hat(X) (model estimate)")
+            ax.scatter(x_vals[:, 0],
+                       x_vals[:, 1],
                        tmp_y,
+                       s=10*np.ones(x_vals[:,0].shape), c=range(NUM_SAMPLES**2),
                        label="f_hat(X) (model estimate)")
             x1 = X_orig[0, :].reshape(-1, 1)
             x2 = X_orig[1, :].reshape(-1, 1)
@@ -125,14 +123,17 @@ for epoch in range(num_epochs):
             tmp = xscaler.inverse_transform(X_orig[0:NUM_INPUT_FEATURES, :].reshape(-1, NUM_INPUT_FEATURES))
             new_y = y#yscaler.inverse_transform(y)
             new_y.reshape(NUM_SAMPLES, NUM_SAMPLES)
-            #ax.scatter(tmp[:, 0], tmp[:, 1], new_y, label="f(X) (truth data)")
+            ax.scatter(tmp[:, 0], tmp[:, 1], new_y, label="f(X) (truth data)")
             plt.xlabel("X")
             plt.ylabel("Y")
             plt.title(f"y=sin(x) @ epoch {epoch}")
             plt.legend()
             #plt.show()
             plt.savefig(os.path.join("results", f"epoch_{epoch}"))
-            plt.close()
+            if epoch == num_epochs-1:
+                plt.show()
+            else:
+                plt.close()
 
 # Append all the images into a list for imageio
 for file in os.listdir("results"):
