5b1245fe58d77f6b1813dee0f7fe9125e04568c8
Bob Scharmann <bobby.scharmann@gmail.com>
2020-06-06 11:23:24 -0500

    Implementation of tabular Q learning for OpenAI Gym FrozenLake

--- flypy/reinforcement_learning/q_learning/frozenlake.py
+++ flypy/reinforcement_learning/q_learning/frozenlake.py
@@ -8,6 +8,7 @@ GAMMA = 0.9
 
 # Learning Rate - helps the Q table converge smoothly
 ALPHA = 0.2
+TEST_EPISODES = 20
 
 
 class Agent:
@@ -33,7 +34,7 @@ class Agent:
 
         # For each possible action we can transition to from this state
         for action in range(self.env.action_space.n):
-            # Compute teh action value
+            # Compute the action value
             action_value = self.values[(state, action)]
 
             # Update the best value and action if this is the best one
@@ -41,6 +42,13 @@ class Agent:
                 best_value = action_value
                 best_action = action
         return best_value, best_action
+
+    def value_update(self, state, action, reward, next_state):
+        """Update the vaue table from taking a step from the environment and applying the learning rate."""
+        best_value, _ = self.best_value_and_action(next_state)
+        new_value = reward + GAMMA * best_value
+        old_value = self.values[(state, action)]
+        self.values[(state, action)] = old_value * (1 - ALPHA) + new_value * ALPHA
 
     def play_episode(self, env):
         """Play an episode in the environment. This initial state is set based on resetting the environment.
@@ -52,14 +60,10 @@ class Agent:
         state = env.reset()
         while True:
             # Select the best action for the current state
-            action = self.select_action(state)
+            _, action = self.best_value_and_action(state)
 
             # Take that action
             new_state, reward, is_done, _ = env.step(action)
-
-            # Update the reward table
-            self.reward_table[(state, action, new_state)] = reward
-            self.transition_table[state, reward][new_state] += 1
 
             # Accumulate the total reward
             total_reward += reward
@@ -71,28 +75,20 @@ class Agent:
 
         return total_reward
 
-    def value_iteration(self):
-        """This function will perform value iteration method on the environment."""
-        # For every possible state in the observation space
-        for state in range(self.env.observation_space.n):
-            # Compute the values for each possible action
-            state_values = [self.calc_action_value(state, action) for action in range(self.env.action_space.n)]
-
-            # Update the value table for this state based on the maximum value received from transitioning to another
-            # state
-            self.value_table[state] = max(state_values)
 
 
 if __name__ == "__main__":
     test_env = gym.make('FrozenLake-v0')
     agent = Agent()
-    writer = SummaryWriter(comment="-v-iteration")
+    writer = SummaryWriter(comment="-q-learning")
     iter_no = 0
     best_reward = 0.0
     while True:
         iter_no += 1
-        agent.play_n_random_steps(100)
-        agent.value_iteration()
+        agent.sample_env()
+        state, action, reward, next_state = agent.sample_env()
+        agent.value_update(state, action, reward, next_state)
+
         reward = 0.0
         for _ in range(TEST_EPISODES):
             reward += agent.play_episode(test_env)
@@ -106,5 +102,5 @@ if __name__ == "__main__":
             print(f"Solved in {iter_no} iterations.")
             iter_no = 0
 
-    writer.close()
+        writer.close()
 
