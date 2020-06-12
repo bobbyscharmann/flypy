01976ee3caa5aa850a6417c288c81aed2c5c0826
Bob Scharmann <bobby.scharmann@gmail.com>
2020-06-11 22:47:15 -0500

    Finished max and skip env

--- flypy/reinforcement_learning/dqn/atari_pong/wrappers/max_and_skip.py
+++ flypy/reinforcement_learning/dqn/atari_pong/wrappers/max_and_skip.py
@@ -7,31 +7,29 @@ import collections
 
 
 class MaxAndSkipEnv(gym.Wrapper):
+    """Return every skip-th frame."""
     def __init__(self, env=None, skip=4):
         super(MaxAndSkipEnv, self).__init__(env)
         self._obs_buffer = collections.deque(maxlen=2)
         self._skip = skip
 
     def step(self, action):
+        """Repeat action, sum reward, and max over last observations."""
         total_reward = 0.0
-        done = None
+        info = done = None
         for _ in range(self._skip):
             obs, reward, done, info = self.env.step(action)
             self._obs_buffer.append(obs)
             total_reward += reward
             if done:
                 break
-            max_frame = np.max(np.stack(self._obs_buffer), axis=0)
-            return max_frame, total_reward, done, info
+        max_frame = np.max(np.stack(self._obs_buffer), axis=0)
+
+        return max_frame, total_reward, done, info
+
     def reset(self):
-        self.env.reset()
-
-        obs, _, done, _ = self.env.step(1)
-        if done:
-            self.env.reset()
-
-        obs, _, done, _ = self.env.step(2)
-        if done:
-            self.env.reset()
+        self._obs_buffer.clear()
+        obs = self.env.reset()
+        self._obs_buffer.append(obs)
 
         return obs
