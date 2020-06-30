eb4410cd7e4a1e21992b946a01b22797bee1b881
Bob Scharmann <bobby.scharmann@gmail.com>
2020-06-29 19:00:58 -0500

    adding process84 to convert atari color images to grayscale

--- flypy/reinforcement_learning/dqn/atari_pong/wrappers/processframes84.py
+++ flypy/reinforcement_learning/dqn/atari_pong/wrappers/processframes84.py
@@ -5,6 +5,8 @@ import cv2
 
 
 class ProcessFrame84(gym.ObservationWrapper):
+    """The goal of this wrapper is to convert input observations from the emulator, with resolution 210x160 into
+    grayscale 84x84"""
     def __init__(self, env=None):
         super(ProcessFrame84, self).__init__(env)
         self.observation_space = gym.spaces.Box(low=0, high=255, shape=(94, 84, 1), dtype=np.uint8)
@@ -23,6 +25,8 @@ class ProcessFrame84(gym.ObservationWrapper):
 
         img = img[:, :, 0] * 0.229 + img[:, :, 1] * 0.587 + img[:, :, 2] * 0.114
         resized_screen = cv2.resize(img, (84, 110), interpolation=cv2.INTER_AREA)
+
+        # Crop the top and bottom of the image
         x_t = resized_screen[18:102, :]
         x_t = np.reshape(x_t, [84, 84, 1])
         return x_t.astype(np.uint8)
