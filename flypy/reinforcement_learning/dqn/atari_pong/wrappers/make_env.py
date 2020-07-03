47ebcb2326f590b1ff8d7c6f4ffd9455751c7a8b
Bob Scharmann <bobby.scharmann@gmail.com>
2020-07-03 07:37:45 -0500

    added four buffer frames for Atari Gym environment

--- flypy/reinforcement_learning/dqn/atari_pong/wrappers/make_env.py
+++ flypy/reinforcement_learning/dqn/atari_pong/wrappers/make_env.py
@@ -9,11 +9,12 @@ from .scaled_float_frame import ScaledFloatFrame
 
 
 def make_env(env_name):
+    """This function is used to make a gym environment while applying a series of wrappers """
     env = gym.make(env_name)
     env = MaxAndSkipEnv(env)
     env = FireResetEnv(env)
     env = ProcessFrame84(env)
     env = ImageToPyTorch(env)
-    env = BufferWrapper(env)
+    env = BufferWrapper(env, 4)
 
     return ScaledFloatFrame(env)
