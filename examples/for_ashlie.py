96d5c5c219a16409f44e3bff8d1fee953640fbd7
Bob Scharmann <bobby.scharmann@gmail.com>
2020-08-15 15:41:11 -0500

    Script for ashlie

--- examples/for_ashlie.py
+++ examples/for_ashlie.py
@@ -4,7 +4,7 @@ count = 0
 for j in range(1, n+1):
     count = count + 1
     print("outer")
-    for i in range(1, j):
+    for i in range(1, j+1):
         count = count + 1
         print('inner')
 print(f"Count is: {count} when m is {n}")
