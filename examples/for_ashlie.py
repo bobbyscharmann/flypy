a6f6cfea7fa2d889924d10bc1c5fc499d5d3865a
Bob Scharmann <bobby.scharmann@gmail.com>
2020-08-15 15:22:40 -0500

    Script for ashlie

---
+++ examples/for_ashlie.py
@@ -0,0 +1,10 @@
+n = 2
+
+count = 0
+for j in range(1, n+1):
+    count = count + 1
+    print("outer")
+    for i in range(1, j):
+        count = count + 1
+        print('inner')
+print(f"Count is: {count} when m is {n}")
